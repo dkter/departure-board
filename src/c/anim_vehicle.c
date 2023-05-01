d3650f6b32300313c99fcae4f38f0dba7f112feb
David Teresi <dkteresi@gmail.com>
2023-05-01 15:43:12 -0400

    reset vehicle scroll animations when starting a new one

--- src/c/anim_vehicle.c
+++ src/c/anim_vehicle.c
@@ -5,10 +5,20 @@
 static const uint32_t VEHICLE_SCROLL_DURATION = 240;
 static const int16_t VEHICLE_SCROLL_DIST = 60;
 
-static Animation* out_anim;
-static Animation* in_anim;
+static Animation* out_anim = NULL;
+static Animation* in_anim = NULL;
 
 Animation *create_vehicle_outbound_anim(ScrollDirection direction, Layer* vehicle_layer) {
+    if (animation_is_scheduled(out_anim)) {
+        animation_unschedule(out_anim);
+
+        // reset to `from` point
+        GPoint origin;
+        property_animation_get_from_gpoint((PropertyAnimation*)in_anim, &origin);
+        GRect bounds = layer_get_bounds(vehicle_layer);
+        layer_set_bounds(vehicle_layer, GRect(origin.x, origin.y, bounds.size.w, bounds.size.h));
+    }
+
     const int16_t to_dy = (direction == ScrollDirectionDown) ? -VEHICLE_SCROLL_DIST : VEHICLE_SCROLL_DIST;
 
     GPoint to_origin = GPoint(0, to_dy);
@@ -20,6 +30,16 @@ Animation *create_vehicle_outbound_anim(ScrollDirection direction, Layer* vehicl
 }
 
 Animation *create_vehicle_inbound_anim(ScrollDirection direction, Layer* vehicle_layer) {
+    if (animation_is_scheduled(in_anim)) {
+        animation_unschedule(in_anim);
+
+        // reset to `to` point
+        GPoint origin;
+        property_animation_get_to_gpoint((PropertyAnimation*)in_anim, &origin);
+        GRect bounds = layer_get_bounds(vehicle_layer);
+        layer_set_bounds(vehicle_layer, GRect(origin.x, origin.y, bounds.size.w, bounds.size.h));
+    }
+
     const int16_t from_dy = (direction == ScrollDirectionDown) ? -VEHICLE_SCROLL_DIST : VEHICLE_SCROLL_DIST;
 
     GPoint from_origin = GPoint(0, from_dy);
